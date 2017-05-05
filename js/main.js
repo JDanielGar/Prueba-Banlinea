35a1f5b3ad2999eee60e08469d3e6f87ce434c76
Daniel Garcia <jdaniel_gar@hotmail.com>
2017-05-05 15:54:22 -0500

    Add logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// DOM interfase
+
 $(window).resize(function() {
     validatePlaceholders();
 });
@@ -25,3 +27,46 @@ function setPlaceholders() {
 }
 
 validatePlaceholders();
+
+// DOM interaction
+
+$(".submit-button").click(function(){
+    if(validateData()){
+        car_brand = $("#car_branding_select option:selected" ).text();
+        car_model = $("#model_car_select option:selected" ).text();
+        car_year = $("#year_car_select option:selected" ).text();
+        email = $("#email_input").val();
+
+        submits.push({
+            "car_brand": car_brand,
+            "car_model" : parseInt(car_model),
+            "car_year" : parseInt(car_year),
+            "email" : email
+        })
+        alertify.alert("Modelo de carro: "+ car_model +"<br> Modelo del carro: " + car_model + "<br> Año: " + car_year + "<br> Email: " + email );
+    }
+});
+
+function validateData(){
+    if($("#car_branding_select").val()==0 || $("#model_car_select").val()==0 || $("#year_car_select").val()==0 || $("#email_input").val()=="" ){
+        alertify.alert("Hace falta llenar campos");
+        return false;
+    }
+    else {
+        if(validateEmail($("#email_input").val())){
+            return true;
+        }
+        else {
+            alertify.alert("Escriba un email valido");
+        }
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+// Data
+
+submits = [];
